a5d7daec6c5a61a4b91b9dd7d0197ef2dc038892
mehicamina <amina.mehic@stu.ibu.edu.ba>
2021-03-30 11:34:50 +0200

    added flight

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c5481b39418f94228e3eb599dc896baf036b8f3e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c5481b39418f94228e3eb599dc896baf036b8f3e',
+    ),
+    'mikecao/flight' => 
+    array (
+      'pretty_version' => 'v1.3.8',
+      'version' => '1.3.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b3120f8db2ae3b358c1c7a412a92d7aa008a57b4',
+    ),
+  ),
+);
